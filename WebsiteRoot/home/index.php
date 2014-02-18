40196d056d8adb29da4c53224a08d93b939f83fd
Mohit Punjabi <me@mohitpunjabi.com>
2014-02-18 20:05:19 +0530

    Added order by on homepage

--- WebsiteRoot/home/index.php
+++ WebsiteRoot/home/index.php
@@ -10,9 +10,10 @@
 <?php
 	$res = $mysqli->query("SELECT * from user_notifications
 						   WHERE user_to = '".$_SESSION['id']."' AND
-						   ISNULL(rec_date)");
+						   ISNULL(rec_date)
+						   ORDER BY send_date DESC");
 
-	if(!$res) drawNotification("No more notifications", "You do not have any unreaed notifications");
+	if(!$res) drawNotification("No more notifications", "You do not have any unread notifications");
 	else {
 		echo "<h2>Unread Notifications</h2>";
 		while($row = $res->fetch_assoc()) {

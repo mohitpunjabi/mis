bcc794b14a60052dbfac62f36247b814460da3a1
Mohit Punjabi <mohitpunjabi@ismu.ac.in>
2014-11-03 16:01:27 +0530

    1. Changed config files to fit my local box

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'Fibonacci1123';
 $db['default']['database'] = 'mis';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';

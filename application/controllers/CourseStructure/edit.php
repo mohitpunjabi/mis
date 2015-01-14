cf847bdbf7d35af3a05e5fed2aef2b348a543aa2
Umang Popli <popli.umang@gmail.com>
2015-01-14 19:35:31 +0530

    Edit Course structure.

---
+++ application/controllers/CourseStructure/edit.php
@@ -0,0 +1,31 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Edit extends MY_Controller
+{
+	function __construct()
+	{
+		parent::__construct(array('deo'));
+		$this->load->library('session');
+		$this->load->model('CourseStructure/basic_model','',TRUE);
+	}
+	public function index()
+	{
+		$data = array();
+		$data["result_course"] = $this->basic_model->get_course();
+		$data["result_branch"] = $this->basic_model->get_branches();
+		$this->drawHeader();
+		$this->load->view('CourseStructure/edit',$data);
+		$this->drawFooter();
+	}
+	
+	public function ViewCourseStructure()
+	{
+		$data = array();
+		$data["result_course"] = $this->basic_model->get_course();
+		$data["result_branch"] = $this->basic_model->get_branches();
+		$this->drawHeader();
+		$this->load->view('CourseStructure/edit',$data);
+		$this->drawFooter();
+	}
+	
+}
--- application/models/CourseStructure/menu_model.php
+++ application/models/CourseStructure/menu_model.php
@@ -11,15 +11,12 @@ class Menu_model extends CI_Model
 	function getMenu()
 	{
 		$menu=array();
-		//auth ==> emp
-
-		//auth ==> deo
+		
 		$menu['deo']=array();
 		$menu['deo']['Course Structure']=array();
 		$menu['deo']["Course Structure"]["Add Course Structure"] = site_url('CourseStructure/add');
-		$menu['deo']["Course Structure"]["Edit Course Structure"] = site_url('CourseStructure/add');
-		$menu['deo']["Course Structure"]["View Employee Details"] = site_url('employee/view');
-		$menu['deo']["Course Structure"]["Validation Requests"] = site_url('employee/validation');
+		$menu['deo']["Course Structure"]["Edit Course Structure"] = site_url('CourseStructure/edit');
+		$menu['deo']["Course Structure"]["Print Course Structure"] = site_url('CourseStructure/print');
 
 		return $menu;
 	}

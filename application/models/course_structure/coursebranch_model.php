1b8836ac26c06d1a6d4e72b1ca86e7bef55c1912
shashank <shashankshekhar.ism@gmail.com>
2015-01-19 20:48:23 +0530

    dept offers

--- application/models/course_structure/coursebranch_model.php
+++ application/models/course_structure/coursebranch_model.php
@@ -5,7 +5,7 @@ class Coursebranch_model extends CI_Model
 	var $table_coursebranch = 'course_branch';
 	var $table_coursestructure = 'course_structure';
 	var $table_elective_group = 'elective_group';
-
+  var $table_deptoffers='dept_offers';
 
 	function __construct()
 	{
@@ -14,9 +14,14 @@ class Coursebranch_model extends CI_Model
 	}
 
 	
-	function insert($cb_details)
+	function insert_coursebranch($cb_details)
 	{
     $this->db->insert($this->table_coursebranch, $cb_details);
+    return $this->db->_error_message(); 
+	}
+  function insert_deptoffers($cb_details)
+	{
+    $this->db->insert($this->table_deptoffers, $cb_details);
     return $this->db->_error_message(); 
 	}
 }

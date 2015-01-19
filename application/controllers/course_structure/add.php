6712753abec80bfa351da6ab0020466744eca141
Mohit Punjabi <me@mohitpunjabi.com>
2015-01-19 17:53:42 +0530

    CS bug fix

--- application/controllers/course_structure/add.php
+++ application/controllers/course_structure/add.php
@@ -10,7 +10,6 @@ class Add extends MY_Controller
 		$this->addJS("course_structure/add.js");
 		$this->addJS("course_structure/edit.js");
 		$this->addCSS("course_structure/cs_layout.css");
-		$this->load->library('session');
 		$this->load->model('course_structure/basic_model','',TRUE);
 	}
 

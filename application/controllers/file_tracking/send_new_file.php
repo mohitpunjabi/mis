01209013a3b81ba9fcd2c01d0e1a42072eac0e47
Ankit Ladhania <ankit@sportuu.com>
2015-01-20 12:54:31 +0530

    local merge

--- application/controllers/file_tracking/send_new_file.php
+++ application/controllers/file_tracking/send_new_file.php
@@ -17,20 +17,20 @@ class Send_new_file extends MY_Controller
 		$this->load->view('file_tracking/send_new_file/send_new_file',$data);
 		$this->drawFooter ();
 	}
-	public function insert_file_details ($file_sub, $rcvd_emp_id, $remarks)
+	public function insert_file_details ($file_id, $file_sub, $rcvd_emp_id, $remarks)
 	{
 		$emp_id = $this->session->userdata('id');
 		$track_num = time();
 		$data = array(
+				'file_id' => $file_id,
 				'file_subject' => $file_sub ,
 				'track_num'=> $track_num ,
 				'start_emp_id' => $emp_id 
 					  );
 		
 		$this->load->model ('file_tracking/file_details', '', TRUE);
-		$arr = $this->file_details->insert($data, $track_num);
-		$file_id = $arr['file_id'];
-	
+		$this->file_details->insert($data);
+		
 		$this->insert_file_move_details ($file_id, $track_num, $rcvd_emp_id,$remarks);
 	}
 	public function insert_move_details ($file_id, $rcvd_emp_id, $remarks)
@@ -39,7 +39,7 @@ class Send_new_file extends MY_Controller
 		$track_num = $this->file_details->get_track_num ($file_id);
 
 		$this->load->model ('file_tracking/file_move_details', '', TRUE);
-   		$this->file_move_details->change_forward_status ($file_id);
+   		$this->file_move_details->change_forward_status ($track_num);
 		
 		$this->insert_file_move_details ($file_id, $track_num, $rcvd_emp_id, $remarks);
 	}
@@ -60,8 +60,8 @@ class Send_new_file extends MY_Controller
 		$this->load->model ('file_tracking/file_move_details', '', TRUE);
 		$this->file_move_details->insert ($data_arr);
 
-		$this->notification->notify ($rcvd_emp_id, "emp", "File is Received","", "receive_file/validate_track_num/".$file_id, "");
+		$this->notification->notify ($rcvd_emp_id, "emp", "Receive File","File ID: ".$file_id, "receive_file/validate_track_num/".$file_id, "");
 		$data_arr2['track_num'] = $track_num;
-		$this->load->view('file_tracking/send_new_file/send_new_file_ajax2', $data_arr2);
+		$this->load->view('file_tracking/send_new_file/notification', $data_arr2);
 	}
 }

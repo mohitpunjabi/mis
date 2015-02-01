74219a1c2430f79eabe1646b04d95da0b4004bc6
Ashwani Chandil <chandil.ashwani@gmail.com>
2015-02-01 19:12:27 +0530

    Added a helper on new UI ui_helper.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('database','javascript','session','authorization'
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('form','url','html');
+$autoload['helper'] = array('form','url','html','ui_helper');
 
 
 /*

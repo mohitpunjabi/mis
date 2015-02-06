03f928d55160216da143c22fef216eceb95c6ff7
Hitesh Sharma <hitesh0901@gmail.com>
2015-02-07 03:37:56 +0530

    supervisor part done

--- application/controllers/complaint/supervisor.php
+++ application/controllers/complaint/supervisor.php
@@ -45,16 +45,22 @@ class Supervisor extends MY_Controller
 	
 	public function update_complaint_details ($complaint_id)
 	{
+		$this->load->model ('complaint/complaint_details', '', TRUE);
 		$status = $this->input->post('status');
-     	$action_taken = $this->input->post('action_taken');
 		$fresh_action = $this->input->post('fresh_action');
-		
+     	$action_taken = $this->complaint_details->get_remarks($complaint_id);
+
 		$date = date('Y/m/d H:i:s');
-		$fresh_action = $date." : ".$fresh_action;
 		
-		/*echo $status."<br>";
+		if ($action_taken == "NA")
+			$fresh_action = $date." : ".$fresh_action;
+		else
+			$fresh_action = $action_taken."<br/>".$date." : ".$fresh_action;
+
+		$this->complaint_details->update_complaint($complaint_id, $status, $fresh_action);		
+		echo $status."<br>";
 		echo $action_taken."<br>";
-		echo $fresh_action."<br>";*/
+		echo $fresh_action."<br>";
 	}
 	
 	public function open_complaint_list($supervisor)
@@ -76,8 +82,8 @@ class Supervisor extends MY_Controller
 			$data_array[$sno][$j++] = $this->user_model->getNameById($row->user_id);
 			$data_array[$sno][$j++] = $row->date_n_time;
 			$data_array[$sno][$j++] = $row->location;
-			$data_array[$sno][$j++] = urldecode($row->location_details);
-			$data_array[$sno][$j++] = urldecode($row->problem_details);
+			$data_array[$sno][$j++] = $row->location_details;
+			$data_array[$sno][$j++] = $row->problem_details;
 			$data_array[$sno][$j++] = $row->remarks;
 			//$data_array[$sno][$j++] = $row->pref_time;
 			$sno++;
@@ -101,23 +107,127 @@ class Supervisor extends MY_Controller
 
 	public function view_closed_complaint($supervisor)
 	{
-		$this->drawHeader ("View Closed Complaint List");
-		$this->load->view('complaint/supervisor/view_closed_complaint');
-		$this->drawFooter ();		
+		$this->load->model ('complaint/complaint_details', '', TRUE);
+		$status = "Closed";
+		$res = $this->complaint_details->complaint_list($status, $supervisor);
+		
+		$this->load->model('user_model', '', TRUE);
+		
+		$total_rows = $res->num_rows();
+		$data_array = array();
+		$sno = 1;
+		foreach ($res->result() as $row)
+		{
+			$data_array[$sno]=array();
+			$j=1;
+			$data_array[$sno][$j++] = $row->complaint_id;
+			$data_array[$sno][$j++] = $this->user_model->getNameById($row->user_id);
+			$data_array[$sno][$j++] = $row->date_n_time;
+			$data_array[$sno][$j++] = $row->location;
+			$data_array[$sno][$j++] = $row->location_details;
+			$data_array[$sno][$j++] = $row->problem_details;
+			$data_array[$sno][$j++] = $row->remarks;
+			//$data_array[$sno][$j++] = $row->pref_time;
+			$sno++;
+		}
+	
+		$data['data_array'] = $data_array;
+		$data['total_rows'] = $total_rows;
+		
+		if ($total_rows == 0)
+		{
+			$this->drawHeader ("Closed Complaint not found");
+			$this->drawFooter();
+		}
+		else 
+		{
+			$this->drawHeader ("Closed Complaint List");
+			$this->load->view('complaint/supervisor/view_closed_complaint', $data);
+			$this->drawFooter();
+		}
 	}
 
 	public function view_rejected_complaint($supervisor)
 	{
-		$this->drawHeader ("View Rejected Complaint List");
-		$this->load->view('complaint/supervisor/view_rejected_complaint');
-		$this->drawFooter ();				
+		$this->load->model ('complaint/complaint_details', '', TRUE);
+		$status = "Rejected";
+		$res = $this->complaint_details->complaint_list($status, $supervisor);
+		
+		$this->load->model('user_model', '', TRUE);
+		
+		$total_rows = $res->num_rows();
+		$data_array = array();
+		$sno = 1;
+		foreach ($res->result() as $row)
+		{
+			$data_array[$sno]=array();
+			$j=1;
+			$data_array[$sno][$j++] = $row->complaint_id;
+			$data_array[$sno][$j++] = $this->user_model->getNameById($row->user_id);
+			$data_array[$sno][$j++] = $row->date_n_time;
+			$data_array[$sno][$j++] = $row->location;
+			$data_array[$sno][$j++] = $row->location_details;
+			$data_array[$sno][$j++] = $row->problem_details;
+			$data_array[$sno][$j++] = $row->remarks;
+			//$data_array[$sno][$j++] = $row->pref_time;
+			$sno++;
+		}
+	
+		$data['data_array'] = $data_array;
+		$data['total_rows'] = $total_rows;
+		
+		if ($total_rows == 0)
+		{
+			$this->drawHeader ("Rejected Complaint not found");
+			$this->drawFooter();
+		}
+		else 
+		{
+			$this->drawHeader ("Rejected Complaint List");
+			$this->load->view('complaint/supervisor/view_rejected_complaint', $data);
+			$this->drawFooter();
+		}
 	}
 
 	public function view_all_complaint($supervisor)
 	{
-		$this->drawHeader ("View All Complaint List");
-		$this->load->view('complaint/supervisor/view_all_complaint');
-		$this->drawFooter ();				
+		$this->load->model ('complaint/complaint_details', '', TRUE);
+		$res = $this->complaint_details->all_complaint_list($supervisor);
+		
+		$this->load->model('user_model', '', TRUE);
+		
+		$total_rows = $res->num_rows();
+		$data_array = array();
+		$sno = 1;
+		foreach ($res->result() as $row)
+		{
+			$data_array[$sno]=array();
+			$j=1;
+			$data_array[$sno][$j++] = $row->complaint_id;
+			$data_array[$sno][$j++] = $row->status;
+			$data_array[$sno][$j++] = $this->user_model->getNameById($row->user_id);
+			$data_array[$sno][$j++] = $row->date_n_time;
+			$data_array[$sno][$j++] = $row->location;
+			$data_array[$sno][$j++] = $row->location_details;
+			$data_array[$sno][$j++] = $row->problem_details;
+			$data_array[$sno][$j++] = $row->remarks;
+			//$data_array[$sno][$j++] = $row->pref_time;
+			$sno++;
+		}
+	
+		$data['data_array'] = $data_array;
+		$data['total_rows'] = $total_rows;
+		
+		if ($total_rows == 0)
+		{
+			$this->drawHeader ("No Complaint found");
+			$this->drawFooter();
+		}
+		else 
+		{
+			$this->drawHeader ("All Complaint List");
+			$this->load->view('complaint/supervisor/view_all_complaint', $data);
+			$this->drawFooter();
+		}
 	}
-
 }

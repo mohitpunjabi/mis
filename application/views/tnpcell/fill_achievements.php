9543794e23523f26c0be620e1e0b3083cab50c55
shashank <shashankshekhar.ism@gmail.com>
2015-02-10 07:51:10 +0530

    USing new UI type

--- application/views/tnpcell/fill_achievements.php
+++ application/views/tnpcell/fill_achievements.php
@@ -1,44 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to T&P Cell</title>
-</head>
-<body>
-
-<div id="container">
-	<h1>Welcome to Training & Placement Cell!</h1>
-  <center>
-  <font face="Arial" size="3">
-  <h3><b>Add details to your CV</b></h3>
-  <?php
-    echo form_open('tnpcell/cv/save_achievements');
-  ?>
-  <h3>Academic/Co-Curricular Achievements</h3>
-  <table align="center">
-    <?php 
-      $category =array("Research Papers Published", "Academic Achievements", "Co-curricular Achievements","Position of Responsibility","Skill-Set");
-      for($i=1;$i<=5;$i++) { 
-    ?>
-    <tr>
-      <td>
-        <?php echo $i ?>
-      </td>
-      <td>
-        <input type="text" name="category<?php echo $i ?>" value ="<?php echo $category[$i-1] ?>" placeholder="<?php echo $category[$i-1] ?>"/>
-      </td>
-      <td>
-        <textarea name="information<?php echo $i ?>" cols="40" rows="5"></textarea>
-      </td>
-    </tr>
-    <?php } ?>
-    </table>
-    <?php
-    echo form_submit('submit', 'Save');
-    echo form_close();
-    ?>
-  </font>
-  </center>
-</div>
-</body>
-</html>
+<?php
+	$ui = new UI();
+    $outer_row = $ui->row()->id('or')->open();
+    $column1 = $ui->col()->width(12)->t_width(6)->m_width(12)->open();
+    
+    echo '<h3><b><center>Add details to your CV</b></center>';
+    echo '2. Awards & Achievements</h3>';
+    $formbox =  $ui->box()->id('box_form')->open();
+                    $form=$ui->form()->id("add_course_form")->action("tnpcell/cv/save_achievements")->open();
+    
+    $table = $ui->table()->responsive()->hover()->bordered()->open();
+    $category =array("Research Papers Published", "Academic Achievements", "Co-curricular Achievements","Position of Responsibility","Skill-Set");
+							echo '
+								  <tr>
+									<th>Title</th>
+                  <th>Details</th>
+								  </tr>';
+    for($i=1;$i<=5;$i++) {
+              echo '
+								  <tr> 
+									<td>';
+                  $ui->input()
+											->placeholder($category[$i-1])
+                      ->value($category[$i-1])
+											->id('category'.$i)
+											->name('category'.$i)
+											->show();
+              echo '
+									</td>
+									<td>';
+                  $ui->textarea()
+                      ->rows(7)
+                      ->cols(60)
+											->placeholder('Max 2000 characters')
+											->id('information'.$i)
+											->name('information'.$i)
+											->show();
+              echo '
+									</td>
+									</tr>  ';
+		}
+    $table->close();
+    echo '<br><center>';
+    $ui->button()
+					->value('Save')
+					->uiType('primary')
+					->submit()
+					->name('Submit')
+					->show();
+    $form->close();
+		$formbox->close();
+				
+		$column1->close();
+	$outer_row->close();
+?>
--- application/views/tnpcell/fill_projects.php
+++ application/views/tnpcell/fill_projects.php
@@ -1,59 +1,71 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to T&P Cell</title>
-</head>
-<body>
+<?php
+	$ui = new UI();
+    $outer_row = $ui->row()->id('or')->open();
+    $column1 = $ui->col()->width(12)->t_width(6)->m_width(12)->open();
+    
+    echo '<h3><b><center>Add details to your CV</b></center>';
+    echo '1. Project/Internship/Excursion/Training</h3>';
+    $formbox =  $ui->box()->id('box_form')->open();
+                    $form=$ui->form()->id("add_course_form")->action("tnpcell/cv/save_projects")->open();
+    
+    $table = $ui->table()->responsive()->hover()->bordered()->open();
+							echo '
+								  <tr>
+									<th>Sl.No</th>
+                  <th>Details</th>
+								  </tr>';
+    for($counter=1;$counter<=5;$counter++) {
+              echo '
+								  <tr> 
+									<td>';
+                  echo $counter;
+              echo '
+									</td>
+									<td>';
+                  $ui->input()
+											->placeholder('Place')
+											->id('place'.$counter)
+											->name('place'.$counter)
+											->show();
 
-<div id="container">
-	<h1>Welcome to Training & Placement Cell!</h1>
-  <center>
-  <font face="Arial" size="3">
-  <h3><b>Add details to your CV</b></h3>
-  <?php
-    echo form_open('tnpcell/cv/save_projects');
-  ?>
-  <center>
-  <h3>Project/Internship/Excursion/Training</h3>
-  <table align="center">
-    <tr>
-      <th>Sl.No</th>
-      <th>Place</th>
-      <th>Project Title</th>
-      <th>Duration</th>
-      <th>Role</th>
-      <th>Project Description</th>
-    </tr>
-    <?php for($counter=1;$counter<=5;$counter++) { ?>
-    <tr>
-      <td><?php echo $counter; ?></td>
-      <td>
-        <input type="text" name="place<?php echo $counter;?>"/>
-      </td>
-      <td>
-        <input type="text" name="title<?php echo $counter;?>"/>
-      </td>
-      <td>
-        <input type="text" placeholder="In weeks" name="duration<?php echo $counter; ?>"/>
-      </td>
-      <td>
-        <input type="text" name="role<?php echo $counter;?>"/>
-      </td>
-      <td>
-        <textarea name="description<?php echo $counter;?>" cols="40" rows="5"></textarea>
-      </td>
-    </tr>
-    <?php } ?>
-    </table>
-    <?php
-    echo form_submit('submit', 'Next');
-    echo form_close();
-    ?>
-  </center>
-  </font>
-  </center>
-</div>
-
-</body>
-</html>
+                $ui->input()
+											->placeholder('Title')
+											->id('title'.$counter)
+											->name('title'.$counter)
+											->show();
+              
+                $ui->input()
+											->placeholder('Duration (in weeks)')
+											->id('duration'.$counter)
+											->name('duration'.$counter)
+											->show();
+              
+                  $ui->input()
+											->placeholder('Role')
+											->id('role'.$counter)
+											->name('role'.$counter)
+											->show();
+              
+                 $ui->textarea()
+											->placeholder('Description')
+											->id('description'.$counter)
+											->name('description'.$counter)
+											->show();
+              echo '
+									</td>
+									</tr>  ';
+		}
+    $table->close();
+    echo '<br><center>';
+    $ui->button()
+					->value('Next')
+					->uiType('primary')
+					->submit()
+					->name('Submit')
+					->show();
+    $form->close();
+			$formbox->close();
+				
+		$column1->close();
+	$outer_row->close();
+?>

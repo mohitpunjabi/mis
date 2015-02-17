4ac529f5a1608135833961996f5067ca0eb3c890
ShivanshuGoyal <shivanshu@ismu.ac.in>
2015-02-17 09:53:15 +0530

    security officer is added in guard_menu_model

--- application/models/guard/guard_menu_model.php
+++ application/models/guard/guard_menu_model.php
@@ -11,7 +11,31 @@ class Guard_menu_model extends CI_Model
 	function getMenu()
 	{
 		$menu=array();
-		//auth ==> supervisor
+		//auth ==> Security Estate Officer
+		$menu['seo']['Guard Management']=array();
+		$menu['seo']['Guard Management']['Home'] = site_url('guard/home');
+		
+		$menu['seo']['Guard Management']['Duties'] = array();
+		//$menu['seo']['Guard Management']['Duties']['Auto Assignment'] = site_url('guard/duties/auto_assignment');
+		$menu['seo']['Guard Management']['Duties']['Assign Duties'] = array();
+		$menu['seo']['Guard Management']['Duties']['Assign Duties']['Regular'] = site_url('guard/duties/assign_regular');
+		$menu['seo']['Guard Management']['Duties']['Assign Duties']['Overtime'] = site_url('guard/duties/assign_overtime');
+		
+		$menu['seo']['Guard Management']['Duties']['Assign Duty to a Guard'] = site_url('guard/duties/assign_to_a_guard');
+		
+		$menu['seo']['Guard Management']['Duties']['View Duty Chart'] = site_url('guard/duties/view');
+		
+				
+		$menu['seo']['Guard Management']['Manage Guard'] = array();
+		$menu['seo']['Guard Management']['Manage Guard']['Add Guard'] = site_url('guard/manage_guard/add');
+		$menu['seo']['Guard Management']['Manage Guard']['View Existing Guards'] = site_url('guard/manage_guard/view');
+		
+		$menu['seo']['Guard Management']['Manage Post'] = array();
+		$menu['seo']['Guard Management']['Manage Post']['Add Post'] = site_url('guard/manage_post/add');	
+		$menu['seo']['Guard Management']['Manage Post']['View Existing Posts'] = site_url('guard/manage_post/view');	
+		
+		
+		//auth ==> guard supervisor
 		$menu['guard_sup']['Guard Management']=array();
 		$menu['guard_sup']['Guard Management']['Home'] = site_url('guard/home');
 		
@@ -24,15 +48,6 @@ class Guard_menu_model extends CI_Model
 		$menu['guard_sup']['Guard Management']['Duties']['Assign Duty to a Guard'] = site_url('guard/duties/assign_to_a_guard');
 		
 		$menu['guard_sup']['Guard Management']['Duties']['View Duty Chart'] = site_url('guard/duties/view');
-		
-				
-		$menu['guard_sup']['Guard Management']['Manage Guard'] = array();
-		$menu['guard_sup']['Guard Management']['Manage Guard']['Add Guard'] = site_url('guard/manage_guard/add');
-		$menu['guard_sup']['Guard Management']['Manage Guard']['View Existing Guards'] = site_url('guard/manage_guard/view');
-		
-		$menu['guard_sup']['Guard Management']['Manage Post'] = array();
-		$menu['guard_sup']['Guard Management']['Manage Post']['Add Post'] = site_url('guard/manage_post/add');	
-		$menu['guard_sup']['Guard Management']['Manage Post']['View Existing Posts'] = site_url('guard/manage_post/view');	
 		
 		return $menu;
 	}

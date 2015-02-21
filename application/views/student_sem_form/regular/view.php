761c9275c99e4c2eb41275e3784bf71ec8ddd7a4
Rohit <rohitkkrana@gmail.com>
2015-02-21 13:39:11 +0530

    Sem Acdamic and Department view Update

--- application/views/student_sem_form/regular/view.php
+++ application/views/student_sem_form/regular/view.php
@@ -46,13 +46,17 @@
               <th>SUbject Name</th>
           </thead>
           <tbody>
-          	  <?php foreach($subjects as $subject) { ?>
+          	  <?php
+			  		
+				 foreach($subjects as $subject) { 
+				 	foreach($subject as $s) {
+					?>
               <tr>
-                <td><?php echo $subject[0]['sequence']; ?></td>
-                <td><?php echo $subject[0]['sequence']; ?></td>
-                <td><?php echo $subject[0]['name']; ?></td>
+                <td><?php echo $s['sequence']; ?></td>
+                <td><?php echo $s['subject_id']; ?></td>
+                <td><?php echo $s['name'];  ?></td>
               </tr>
-    	  <?php }  foreach($confirm['ele'] as $s) { ?>
+    	  <?php }}   foreach($confirm['ele'] as $s) { ?>
       <tr>
         <td><?php echo $s['sub_seq']; ?></td>
         <td><?php echo $s['sub_seq']; ?></td>

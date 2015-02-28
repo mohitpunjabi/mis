be0114ae6c75c845642d76a71b52116f6b87afc7
Mohit Punjabi <me@mohitpunjabi.com>
2015-02-28 18:42:53 +0530

    Notice module auth bugfix.

--- application/controllers/information/post_notice.php
+++ application/controllers/information/post_notice.php
@@ -2,14 +2,15 @@
 
 class Post_notice extends MY_Controller
 {
+    protected $authIds = array('hod','est_ar','exam_dr','dt','dsw');
 	function __construct()
 	{
-		parent::__construct(array('hod','est_ar','exam_dr','dt','dsw'));
+		parent::__construct($this->authIds);
 	}
 
 	public function index($auth_id='')
 	{
-		if($auth_id =='' || ($auth_id !='hod' && $auth_id !='dt' && $auth_id !='dsw' && $auth_id !='est_ar' && $auth_id !='exam_dr'))
+		if(!in_array($auth_id, $this->authIds))
 		{
 			$this->session->set_flashdata('flashError','Access Denied!'.$auth_id);
 			redirect('home');

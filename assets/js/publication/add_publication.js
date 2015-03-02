9607af5578856f6b1c81ecb0a8b3265fb366fdbf
Anant <iamanantkr@gmail.com>
2015-03-02 17:29:13 +0530

    javascript again updated

--- assets/js/publication/add_publication.js
+++ assets/js/publication/add_publication.js
@@ -262,44 +262,48 @@ function get_publication_type(type)
 }
 function get_authors(type)
 {
-	var xmlhttp = getxmlhttp();
-	xmlhttp.onreadystatechange = function()
-	{
-		if (xmlhttp.readyState == 4 && xmlhttp.status==200)
+	if(type !== ''){
+		var xmlhttp = getxmlhttp();
+		xmlhttp.onreadystatechange = function()
 		{
-			document.getElementById("num_author").innerHTML = xmlhttp.responseText;
-			//$(".loading").hide();
+			if (xmlhttp.readyState == 4 && xmlhttp.status==200)
+			{
+				document.getElementById("num_author").innerHTML = xmlhttp.responseText;
+				//$(".loading").hide();
+			}
 		}
+		xmlhttp.open("POST",site_url("publication/add_publication_ajax/add_authors/"+type),true);
+		xmlhttp.send();
+		return false;
 	}
-	xmlhttp.open("POST",site_url("publication/add_publication_ajax/add_authors/"+type),true);
-	xmlhttp.send();
-	return false;
+	else{
+		document.getElementById("num_author").innerHTML = '';
+	}
 }
 function add_template(type,type1)
 {
-	//alert(type);
 	var xmlhttp = getxmlhttp();
 	xmlhttp.onreadystatechange = function()
 	{
 		if (xmlhttp.readyState == 4 && xmlhttp.status==200)
 		{
 			document.getElementById("other_author"+type1).innerHTML = xmlhttp.responseText;
-			//$(".loading").hide();
+
+			var xmlhttp_ = getxmlhttp();
+			xmlhttp_.onreadystatechange = function()
+			{
+				if (xmlhttp_.readyState == 4 && xmlhttp_.status==200)
+				{
+					document.getElementById("department_name"+type1).innerHTML = xmlhttp_.responseText;
+				}
+			}
+			xmlhttp_.open("POST",site_url("publication/add_publication_ajax/find_department/"+type+"/"+type1),true);
+			xmlhttp_.send();
+			
 		}
 	}
 	xmlhttp.open("POST",site_url("publication/add_publication_ajax/input_authors/"+type+"/"+type1),true);
 	xmlhttp.send();
-	var xmlhttp_ = getxmlhttp();
-	xmlhttp_.onreadystatechange = function()
-	{
-		if (xmlhttp_.readyState == 4 && xmlhttp_.status==200)
-		{
-			document.getElementById("department_name"+type1).innerHTML = xmlhttp_.responseText;
-			//$(".loading").hide();
-		}
-	}
-	xmlhttp_.open("POST",site_url("publication/add_publication_ajax/find_department/"+type+"/"+type1),true);
-	xmlhttp_.send();
 	return false;
 }
 function find_faculty(type,type1)

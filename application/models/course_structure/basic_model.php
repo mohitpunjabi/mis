36defd199643422616e59edb1250998eca71d077
Umang Popli <popli.umang@gmail.com>
2015-03-16 22:00:59 +0530

    honour/Minor Done.Testing Left.

--- application/models/course_structure/basic_model.php
+++ application/models/course_structure/basic_model.php
@@ -11,6 +11,8 @@ class Basic_model extends CI_Model
 	var $table_elective_group = 'elective_group';
 	var $table_course_branch = 'course_branch';
 	var $table_elective_offered = 'elective_offered';
+	var $table_honour_minor_offered = 'honour_minor_offered';
+	var $table_minor_offered = 'minor_offered';
   	var $table_depts = 'departments';
 
 	function __construct()
@@ -204,6 +206,24 @@ class Basic_model extends CI_Model
 		asc, cast(SUBSTRING_INDEX(`sequence`, '.', -1) as decimal) asc");
 		return $query->result();
 	}
+	
+	function select_all_honour_or_minor_subject_by_aggr_id_and_semester($aggr_id,$semester,$course_id,$branch_id)
+	{
+		$query = $this->db->query("SELECT			
+		subjects.id,subject_id,subjects.name,lecture,tutorial,practical,credit_hours,contact_hours,elective,type,
+		course_structure.aggr_id,course_structure.sequence  
+		FROM  
+		subjects 
+		INNER JOIN course_structure ON course_structure.id = subjects.id 
+		INNER JOIN dept_course ON dept_course.aggr_id = course_structure.aggr_id 
+		INNER JOIN course_branch ON course_branch.course_branch_id = dept_course.course_branch_id 
+		WHERE course_structure.aggr_id <= '$aggr_id' 
+		AND course_structure.semester = '$semester' 
+		AND course_branch.course_id = '$course_id' 
+		AND course_branch.branch_id = '$branch_id' 
+		ORDER BY cast(SUBSTRING_INDEX(`sequence`,'.', 1) as decimal) asc, cast(SUBSTRING_INDEX(`sequence`, '.', -1) as decimal) asc");
+		return $query->result();
+	}
 	function select_all_subject_by_aggr_id_and_semester($aggr_id,$semester)
 	{
 		$query = $this->db->query("SELECT * FROM subjects INNER JOIN course_structure ON course_structure.id = subjects.id WHERE course_structure.aggr_id = '$aggr_id' AND 
@@ -249,6 +269,38 @@ class Basic_model extends CI_Model
 		$query = $this->db->query("SELECT aggr_id FROM dept_course INNER JOIN course_branch ON course_branch.course_branch_id = dept_course.course_branch_id WHERE course_branch.course_id = '$course' AND course_branch.branch_id = '$branch' AND aggr_id <= '$expected_aggr_id'  ORDER BY aggr_id DESC");
 		return $query->result();
 	}
+	
+	
+	function select_honour_or_minor_offered($aggr_id,$id)
+	{
+    	$query = $this->db->get_where($this->table_honour_minor_offered,array('aggr_id'=>$aggr_id,'id'=>$id));
+    	if($query->num_rows() > 0)
+			return true;	
+	}
+	
+	function select_honour_or_minor_offered_by_aggr_id($aggr_id,$semester)
+	{
+		$query = $this->db->query("SELECT * FROM honour_minor_offered INNER JOIN course_structure ON course_structure.id = honour_minor_offered.id WHERE honour_minor_offered.aggr_id = '$aggr_id' AND course_structure.semester = '$semester'");
+			return $query->result();	
+	}	
+	
+	function delete_honour_or_minor_offered($aggr_id,$semester)
+	{
+		$query = $this->db->query("DELETE honour_off FROM honour_minor_offered honour_off INNER JOIN course_structure ON course_structure.id = 
+		honour_off.id WHERE honour_off.aggr_id = '$aggr_id' AND course_structure.semester = '$semester'");
+	    
+		if($this->db->affected_rows() >=0 || !$this->db->_error_message())
+			return true;
+
+	} 
+	
+	function insert_honour_or_minor_offered($data)
+	{
+    	$this->db->insert($this->table_honour_minor_offered,$data);
+    	return $this->db->_error_message(); 
+	}
+	
+	
 	
 }
 
